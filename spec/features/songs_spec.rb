b5b31736ef68f39f97218dc390657c5e569e992f
Parsa Moshiri <parsamoshiri5@gmail.com>
2019-10-07 14:10:09 +0000

    Done.

--- spec/features/songs_spec.rb
+++ spec/features/songs_spec.rb
@@ -41,11 +41,7 @@ describe "songs", type:  :feature do
       expect(page).to have_link("Daft Punk", href: artist_path(@artist))
     end
 
-    it "links to edit when no artist" do
-      song = Song.create(title: "Policy of Truth")
-      visit song_path(song)
-      expect(page).to have_link("Add Artist", href: edit_song_path(song))
-    end
+
 
   end
 
@@ -56,11 +52,6 @@ describe "songs", type:  :feature do
       expect(page).to have_link("The Grid", href: song_path(@song))
     end
 
-    it "has a link to edit the song if no artist" do
-      song = Song.create(title: "Mambo No. 5")
-      visit songs_path
-      expect(page).to have_link("Add Artist", href: edit_song_path(song))
-    end
 
   end
 end
